0b44d4062161448d92c86355b0869f20527ddaf5
mumair <mumairofficial@gmail.com>
2022-07-07 13:31:35 +0000

    fix about section

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -9,26 +9,29 @@ const AboutSection = () => {
       <div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
         <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-            About Quran Mahal
+            Our Story
           </h2>
           <p className="mt-6 text-lg tracking-tight text-blue-100">
             All the information about how Quran Mahal performing and how to get!
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 py-4">
-          <div className="flex">
-            <div>icon</div>
-            <div>
-              <div>title</div>
-              <div>description</div>
-            </div>
-          </div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
+        <div className="text-center tracking-wide text-white mt-10 max-w-4xl mx-auto px-12">
+          <p>
+            Quran Mahal produce and place collection boxes in various sizes,
+            color them and place nation wide.
+          </p>
+
+          <p>
+            Each day corresponding team collects then pages and empty out in big
+            cart. After collection small teams clean up the pages and prepare
+            the final bundle. Once all preparation are complete we dispatch
+            bundles into dedicated space we call Quran well.
+          </p>
+
+          <p className="mt-4">
+            You can contribute via money, buying tin box or contributing in
+            fuel.
+          </p>
         </div>
       </div>
     </section>
