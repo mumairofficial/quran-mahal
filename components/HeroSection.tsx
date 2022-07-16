c484dc02072c0f002ebceaf005e2111258bd9e51
Muhammad Umair <mumairofficial@gmail.com>
2022-07-16 17:32:54 +0500

    hero title padding fix

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -20,7 +20,7 @@ const HeroSection = ({ contact }: PropsWithChildren<IProps>) => {
       />
       <div className="absolutue w-full h-full top-0 left-0 "></div>
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
-        <div className="bg-blend-normal bg-indigo-50 w-70 bg-opacity-60 rounded-lg drop-shadow-2xl mx-auto p-12" style={{maxWidth: "600px"}}>
+        <div className="bg-blend-normal bg-indigo-50 w-70 bg-opacity-60 rounded-lg drop-shadow-2xl mx-auto p-8" style={{maxWidth: "600px"}}>
           <h1 className="text-black text-3xl md:text-4xl font-bold">
             Quran Mahal
           </h1>
