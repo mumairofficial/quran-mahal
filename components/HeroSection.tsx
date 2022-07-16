a4ee75355b7223c294564933ba2a70e861fc4252
Muhammad Umair <mumairofficial@gmail.com>
2022-07-16 17:31:02 +0500

    hero section treatment

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -20,7 +20,7 @@ const HeroSection = ({ contact }: PropsWithChildren<IProps>) => {
       />
       <div className="absolutue w-full h-full top-0 left-0 "></div>
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
-        <div className="bg-blend-normal bg-indigo-50 w-70 opacity-60 rounded-lg drop-shadow-2xl">
+        <div className="bg-blend-normal bg-indigo-50 w-70 bg-opacity-60 rounded-lg drop-shadow-2xl mx-auto p-12" style={{maxWidth: "600px"}}>
           <h1 className="text-black text-3xl md:text-4xl font-bold">
             Quran Mahal
           </h1>
@@ -30,7 +30,7 @@ const HeroSection = ({ contact }: PropsWithChildren<IProps>) => {
           <h3 className="text-black">
             To Donate Collection Box or make small contribution{" "}
           </h3>
-          <button className="bg-black rounded-lg py-4 px-4">
+          <button className="bg-black rounded-lg py-4 px-4 mt-4">
             Call us at +92-348-6317823
           </button>
         </div>
